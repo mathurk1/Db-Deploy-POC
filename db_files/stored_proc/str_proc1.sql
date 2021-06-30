e824b307f6bf974f5ae0595461ed4690c02806cc
Karthik Mathur <22126205+mathurk1@users.noreply.github.com>
2021-06-30 11:33:30 -0500

    alter proc to select get date

--- db_files/stored_proc/str_proc1.sql
+++ db_files/stored_proc/str_proc1.sql
@@ -1,10 +1,12 @@
-CREATE PROCEDURE [dbo].[str_proc1]
+CREATE OR ALTER PROCEDURE [dbo].[str_proc1]
 AS
 BEGIN
 SET NOCOUNT ON
  
     insert into dbo.test_table_new
     select * 
-    from dbo.test_table
+    from dbo.test_table;
+    
+    SELECT getdate();
  
 END
