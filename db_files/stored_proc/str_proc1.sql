e607a04959c51111a9bca257195430d5af0b7524
Karthik Mathur <22126205+mathurk1@users.noreply.github.com>
2021-06-30 10:15:37 -0500

    Update str_proc1.sql

--- db_files/stored_proc/str_proc1.sql
+++ db_files/stored_proc/str_proc1.sql
@@ -1,4 +1,9 @@
-CREATE PROCEDURE str_proc1
+/** MSSQL Stored Proc **/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[str_proc1]
 AS
 BEGIN
 SET NOCOUNT ON
@@ -8,3 +13,4 @@ SET NOCOUNT ON
     from dbo.test_table
  
 END
+GO
